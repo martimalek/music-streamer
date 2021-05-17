2b66ab9b681445c4d111e4899dd12ae36df5bee2
martimalek <martimalekdot@hotmail.es>
2021-05-17 16:26:09 +0200

    - Renamed audioFromUrl to getAudioFromUrl resolver - Added path attribute to Audio

--- src/infrastructure/graphql/resolvers.ts
+++ src/infrastructure/graphql/resolvers.ts
@@ -4,10 +4,16 @@ import { fromUrlDownloader } from './dependencies';
 
 export const resolvers: IResolvers = {
     Query: {
-        async audioFromUrl(_, { url }) {
+        async getAudioFromUrl(_, { url }) {
             const audio = await fromUrlDownloader.execute(url);
 
             return audio;
+
+            /**
+             * - Check whether the audio is already downloaded
+             * - If it is retrieve it and send the title (in the future stream it)
+             * - If it is not then it should download it and persist it as downloaded
+             */
         },
     },
 };
--- src/modules/Audio/Domain/Audio.ts
+++ src/modules/Audio/Domain/Audio.ts
@@ -3,6 +3,7 @@ import { AggregateRoot } from '@modules/Shared/Domain/AggregateRoot';
 export class Audio extends AggregateRoot {
     constructor(
         public url: string,
+        public path: string,
     ) {
         super();
     }
